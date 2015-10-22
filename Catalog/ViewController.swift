3021518190cfc3a44277dc48fa1fb9c3ff1382fd
hongjungah <hong@hongjeongaui-MacBook-Air.local>
2015-10-22 22:01:37 +0900

    [20151022] 장바구니 추가

--- Catalog/ViewController.swift
+++ Catalog/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, ProductCellDelegate {
 
     var data: [String]!
+    var cartList = [String]()
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,7 +24,7 @@ class ViewController: UIViewController, UITableViewDataSource, ProductCellDelega
             "golf",
             "rugby",
             "shuttlecock",
-            "tennis",
+            "pingpong",
             "volleyball",
             "tennis",
             "iceHockey",
@@ -35,28 +37,64 @@ class ViewController: UIViewController, UITableViewDataSource, ProductCellDelega
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Catalog"
+        case 1:
+            return "Cart"
+        default:
+            return ""
+        }
+    }
+    
     func addCart(productCode: String) {
-        print("\(productCode)")
+        cartList.insert(data[Int(productCode)!], atIndex: cartList.count)
+        tableView.reloadData()
+        
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        if section == 0 {
+            return data.count
+        }
+        else {
+            return cartList.count
+        }
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("PRODUCT_CELL", forIndexPath: indexPath) as! ProductCell
         
-        let itemName = data[indexPath.row]
-        print(itemName)
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("PRODUCT_CELL", forIndexPath: indexPath) as! ProductCell
         
-        cell.productName.text = itemName
-        let image = UIImage(named: "\(itemName).png")
-        cell.productImage.image = image
-        cell.productPrice.text = "\(indexPath.row+1)00"
+            let itemName = data[indexPath.row]
+            print(itemName)
         
-        cell.delegate = self
+            cell.productName.text = itemName
+            let image = UIImage(named: "\(itemName).png")
+            cell.productImage.image = image
+            cell.productPrice.text = "\(indexPath.row+1)00"
         
-        return cell
+            let code = indexPath.row.description
+            cell.productCode = code
+
+            cell.delegate = self
+            
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("CART_CELL", forIndexPath: indexPath)
+        
+            cell.textLabel?.text = cartList[cartList.count-indexPath.row-1]
+        
+            return cell
+        }
     }
 }
 
